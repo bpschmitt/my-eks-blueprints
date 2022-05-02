2d4fe87abfd32ef380b0cf09469ee6776ffb3d28
Brad Schmitt <bschmitt@newrelic.com>
2022-05-01 20:48:11 -0500

    nr cluster names

--- bin/my-eks-blueprints.ts
+++ bin/my-eks-blueprints.ts
@@ -2,7 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import ClusterConstruct from '../lib/my-eks-blueprints-stack';
 import PipelineConstruct from '../lib/pipeline';
 
-
 const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT!;
 const region = process.env.CDK_DEFAULT_REGION;
--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,15 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
+import { NewRelicAddOn } from '@newrelic/newrelic-eks-blueprints-addon';
 
 export default class PipelineConstruct extends Construct {
   constructor(scope: Construct, id: string, props?: cdk.StackProps){
     super(scope,id)
 
-    const blueprint = blueprints.EksBlueprint.builder()
+    const blueprintDev = blueprints.EksBlueprint.builder()
     .account(props?.env?.account)
     .region(props?.env?.region)
-    .addOns()
+    .addOns(new blueprints.SecretsStoreAddOn)
+    .addOns(new NewRelicAddOn({
+      newRelicClusterName: "my-eks-blueprints-workshop-dev",
+      awsSecretName: "my-eks-blueprints-workshop"
+    }))
+    .teams();
+
+    const blueprintProd = blueprints.EksBlueprint.builder()
+    .account(props?.env?.account)
+    .region(props?.env?.region)
+    .addOns(new blueprints.SecretsStoreAddOn)
+    .addOns(new NewRelicAddOn({
+      newRelicClusterName: "my-eks-blueprints-workshop-prod",
+      awsSecretName: "my-eks-blueprints-workshop"
+    }))
     .teams();
 
     blueprints.CodePipelineStack.builder()
@@ -23,8 +38,8 @@ export default class PipelineConstruct extends Construct {
       .wave({
         id: "envs",
         stages: [
-          { id: "dev", stackBuilder: blueprint.clone('us-east-1')},
-          { id: "prod", stackBuilder: blueprint.clone('us-west-2')}
+          { id: "dev", stackBuilder: blueprintDev.clone('us-east-1')},
+          { id: "prod", stackBuilder: blueprintProd.clone('us-west-2')}
         ]
       })
       .build(scope, id+'-stack', props);
